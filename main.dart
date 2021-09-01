133b1313e1fd668702a9d5cd40af8047bcc94c62
Suhas157 <49067655+Suhas157@users.noreply.github.com>
2021-09-01 22:08:39 +0530

    Change color Assignment 1

--- main.dart
+++ main.dart
@@ -1,28 +1,63 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    // create material app
-      MaterialApp(
-        //create home widget
-        home:Scaffold(
-          backgroundColor: Colors.blueGrey[100],
-          // appBar is a property here and AppBar is widget here
-          body: Center(
-            child: Image(
-
-              image: NetworkImage('https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510_960_720.jpg'),
-              //image: AssetImage('images/26.jpg'),
-            ),
-          ),
-          appBar:AppBar(
-            title:Text('I am Rich'),
-            backgroundColor: Colors.blueGrey[900],
-          ),
-
-        )
-      )
-
-  );
+  runApp(MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primaryColor: Color(0xff00363a),
+      ),
+      home: MyHomePage(title: 'Hi this is Suhas!'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool _hasBeenPressed = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor:
+            _hasBeenPressed ? Color(0xfff44336) : Color(0xff689f38),
+        appBar: AppBar(
+          title: Text(widget.title),
+          backgroundColor: Color(0xff00363a),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Good Morning!',
+              ),
+            ],
+          ),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {
+            setState(() {
+              _hasBeenPressed = !_hasBeenPressed;
+            });
+          },
+          label: const Text('Change color'),
+          foregroundColor: Color(0xfff3e5f5),
+          backgroundColor: Color(0xff00363a),
+        ));
+  }
+}
